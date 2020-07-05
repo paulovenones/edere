450d779bc354516a4ed07dbf4115704fb4e28420
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-07-05 09:39:16 -0300

    Some files deleted

--- src/stages/99-future-options.js
+++ src/stages/99-future-options.js
@@ -6,23 +6,6 @@
 //     menu += `Posição: ${value} - ` + `Descrição: ${element.descricao} - ` + `R$ ${element.preco}\n`;
 // });
 
-// if (message === "cancelar") {
-//     banco.db[user].stage = 1;
-//     return [
-//         "Okay, já joguei tudo fora.",
-//         "Quando estiver pronto, digita *continuar*, beleza?"
-//     ];
-// }
-
-// if (message === "confirmar") {
-//     banco.db[user].stage = 3;
-//     return [
-//         "Ótimo!",
-//         "Agora, digita o seu endereço pra gente já ir se preparando."
-//     ];
-// }
-
-
 // let total = 0;
 // banco.db[user].itens.forEach(value => {
 //     total += value.preco;
@@ -34,32 +17,11 @@
 //     "Ah, e você pode cancelar tudo digitando *cancelar*."
 // ]
 
-// --
-
-
-
 // if (message === "cancelar") {
 //     database.db[user].stage = 0;
 //     return [
 //         "Okay, já joguei tudo fora.",
 //         "Caso queira começar de novo, só falar comigo."
-//     ];
-// }
-
-// if (message === "terminei") {
-//     database.db[user].stage = 2;
-//     return [
-//         "Pronto, tudo anotado aqui!",
-//         "Só digita *confirmar* pra gente continuar, pode ser? Isso é só um bug, mas já já a gente conserta! :D"
-//     ];
-// }
-
-// if (!cardapio.menu[message]) {
-//     return [
-//         "Mmmm... não temos essa opção por aqui. Poderia digitar novamente? " +
-//         "Ó, você só precisa digitar o número desse jeito, por exemplo: ",
-//         "2",
-//         "Certo? Então, vamos tentar mais uma vez! Pode digitar sua opção ;)"
 //     ];
 // }
 
@@ -71,3 +33,11 @@
 //     "Caso queria finalizar, pode digitar *terminei*, tá?",
 //     "Ah, e você pode cancelar tudo digitando *cancelar*."
 // ];
+
+// if (message === "cancelar") {
+//     banco.db[user].stage = 2;
+//     return [
+//         "Eu ainda nem tinha anotado! haha",
+//         "Então, qual era mesmo o endereço?"
+//     ];
+// }
