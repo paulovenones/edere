02a36022cc79a4dc3d5ab9e109ae6b910014ef42
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-07-05 17:24:11 -0300

    touches

--- src/stages/02-entrepreneur/02-get-address.js
+++ src/stages/02-entrepreneur/02-get-address.js
@@ -2,16 +2,14 @@ const { findClient, nextStage } = require("../../app/utils");
 
 function execute(user, message) {
     foundClient = findClient(user);
-    let addressLineArray = message.trim().split(",");
+    foundClient.storeName = message.trim();
 
     nextStage(foundClient);
 
     return [
-        "Anotado. Nesse passo você vamos focar nas informações do seu estabelecimento" +
-        "Conte, ele é um… (exemplo: restaurante)"+
-
+        "Maravilha. Preciso agora do endereço completo do seu estabelecimento, " +
+        "com a rua, número, bairro e cidade. Olha, é só digitar como nesse exemplo:",
         "Rua Nome da Rua, 123 - Nome do Bairro, Cidade"
-
     ];
 
 }
